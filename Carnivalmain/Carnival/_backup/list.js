dfac108c214788b600ccf695f16c3c4c595569b8
Shashwat Shankar <jeshugames2@gmail.com>
2019-01-20 23:55:15 +0530

    Add files via upload

---
+++ Carnivalmain/Carnival/_backup/list.js
@@ -0,0 +1,19 @@
+
+		var voters ={
+				"9898989898":{
+							username: "yohaan",
+							pin: "town",
+							number: "9898989898",
+							money: 500
+						}
+			};
+
+		var ideas = {
+
+				aqua:{
+					codename: "aqua",
+					funding: 0
+
+				}
+
+			};
